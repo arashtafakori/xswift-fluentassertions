bd2255b6afa01fe53b7e54778c8d21353ad32db3
arashtafakori <arash.software.dev@gmail.com>
2023-09-27 15:05:17 +0300

    this repository has been separated from the corex repository and is an independent repository and the FluentAssertions.Xswift nuget is being made directly from this repository.

---
+++ AssertionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using XSwift.Base;
+using FluentAssertions.Specialized;
+using MassTransit.Initializers;
+
+namespace FluentAssertions.XSwift
+{
+    public static class AssertionExtensions
+    {
+        public static async Task BeSatisfiedWith<TIssue>(
+            this NonGenericAsyncFunctionAssertions nonGenericAsyncFunctionAssertions) where TIssue : IIssue
+        {
+            (await nonGenericAsyncFunctionAssertions.
+                ThrowExactlyAsync<ErrorException>().
+                Select(x => x.Which.Error.Issues.OfType<TIssue>()
+                .Count())).Should().BeGreaterThan(0);
+        }
+    }
+}
