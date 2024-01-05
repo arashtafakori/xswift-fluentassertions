3fa563673ba0067f5f976e63e1da96623c3333d5
arashtafakori <arashtafakori83@gmail.com>
2024-01-05 16:28:24 +0300

    All classess in their methodes and fields were documented. Some tests were implemented for the SUT.

--- src/AssertionExtensions.cs
+++ src/AssertionExtensions.cs
@@ -1,18 +1,32 @@
 ﻿using XSwift.Base;
 using FluentAssertions.Specialized;
-using MassTransit.Initializers;
+using FluentAssertions;
 
-namespace FluentAssertions.XSwift
+namespace XSwift.FluentAssertions
 {
+    /// <summary>
+    /// Provides extension methods for assertions related to asynchronous functions.
+    /// </summary>
     public static class AssertionExtensions
     {
+        /// <summary>
+        /// Asserts that the asynchronous function throws an ErrorException containing an issue of the specified type.
+        /// </summary>
+        /// <typeparam name="TIssue">The type of the issue to check for in the ErrorException.</typeparam>
+        /// <param name="nonGenericAsyncFunctionAssertions">The NonGenericAsyncFunctionAssertions instance.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        /// <remarks>
+        /// This method is used in conjunction with FluentAssertions for asserting that an asynchronous function
+        /// throws an ErrorException containing an issue of the specified type.
+        /// </remarks>
         public static async Task BeSatisfiedWith<TIssue>(
             this NonGenericAsyncFunctionAssertions nonGenericAsyncFunctionAssertions) where TIssue : IIssue
         {
-            (await nonGenericAsyncFunctionAssertions.
-                ThrowExactlyAsync<ErrorException>().
-                Select(x => x.Which.Error.Issues.OfType<TIssue>()
-                .Count())).Should().BeGreaterThan(0);
+            var exception = await nonGenericAsyncFunctionAssertions.ThrowExactlyAsync<ErrorException>();
+
+            exception.Which.Error.Issues
+                .OfType<TIssue>()
+                .Should().NotBeEmpty($"because there should be at least one {typeof(TIssue).Name} issue");
         }
     }
 }
